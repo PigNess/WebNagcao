68236ceaeb3d682b5afc83d198f3dccc1fbf7df9
PigNess <lequanganhtuan1411@gmail.com>
2026-04-04 23:42:02 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,14 @@
+const MyConstants = {
+  DB_SERVER:'tramhuong.zny48hk.mongodb.net',
+  DB_USER: 'chixeom0506_db_user',
+  DB_PASS: 'CHI123',
+  DB_DATABASE: 'TramHuong',
+
+  EMAIL_USER: 'chixeom0506@gmail.com', // Microsoft mail service
+  EMAIL_PASS: 'ignnwilfdtzzzjrc',
+
+  JWT_SECRET: 'abciooiqwe',
+  JWT_EXPIRES: '356658435644111' // in milliseconds
+};
+
+module.exports = MyConstants;
